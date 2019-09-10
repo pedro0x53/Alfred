d36173d81c2dd9358aed4ff8b347ed483f3460ae
Pedro Sousa <pedrosousa2708@gmail.com>
2019-09-10 15:31:13 -0300

    gitignore updated

---
+++ app/src/main/java/assistant/alfred/MainActivity.java
@@ -0,0 +1,17 @@
+package assistant.alfred;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+}
