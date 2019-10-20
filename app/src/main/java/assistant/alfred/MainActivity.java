a0c9ab1f4059e4fd2cee04a10f39033cbaff570c
Pedro Sousa <pedrosousa2708@gmail.com>
2019-10-20 14:15:40 -0300

    Criação da função "ouvir", que é disparada ao pressionar o botão do microfone. O output é o próprio texto que foi falado.

--- app/src/main/java/assistant/alfred/MainActivity.java
+++ app/src/main/java/assistant/alfred/MainActivity.java
@@ -1,17 +1,53 @@
 package assistant.alfred;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.view.View;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv = findViewById(R.id.mainResponse);
+        tv.setText("");
+
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        tv = findViewById(R.id.mainResponse);
+        if (resultCode == RESULT_OK && null != data){
+            ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            int size = result.size();
+            if(size < 0) {
+                for (int i = 0; i < result.size(); i++) {
+                    tv.setText(result.get(i));
+                }
+            }
+        }
+    }
 
+    public void ouvir(View view){
+        try {
+            Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+            //intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "pt-BR");
+            startActivityForResult(intent, 10);
+        }catch (Exception e){
+            tv = findViewById(R.id.mainResponse);
+            tv.setText(e.getMessage());
+        }
+    }
 
 }
