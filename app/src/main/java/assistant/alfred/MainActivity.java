7097e51babaa76ec34f7f6df04092ebca15fc48f
Pedro Sousa <pedrosousa2708@gmail.com>
2020-01-03 13:23:34 -0300

    Remoção da função "ouvir", que agora é definida pelo onClick no ImageButton "mic". onActivityResult agora chama o método "processar" da classe Assistente.

--- app/src/main/java/assistant/alfred/MainActivity.java
+++ app/src/main/java/assistant/alfred/MainActivity.java
@@ -1,53 +1,51 @@
 package assistant.alfred;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
+import android.content.Intent;
 import android.speech.RecognizerIntent;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
-
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    TextView tv;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tv = findViewById(R.id.mainResponse);
+        TextView tv = findViewById(R.id.mainResponse);
         tv.setText("");
 
+        ImageButton mic = findViewById(R.id.mic);
+        mic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "pt-BR");
+                startActivityForResult(intent, 10);
+            }
+        });
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
         super.onActivityResult(requestCode, resultCode, data);
-        tv = findViewById(R.id.mainResponse);
-        if (resultCode == RESULT_OK && null != data){
-            ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-            int size = result.size();
-            if(size < 0) {
-                for (int i = 0; i < result.size(); i++) {
-                    tv.setText(result.get(i));
+
+        TextView tv = findViewById(R.id.mainResponse);
+        Assistente a = new Assistente();
+
+        if (resultCode == RESULT_OK && data != null){
+            ArrayList<String> res = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            try {
+                if (!(res.isEmpty())) {
+                    a.processar(res, tv);
                 }
+            }
+            catch (NullPointerException e) {
+                System.out.println(e.getMessage());
             }
         }
     }
-
-    public void ouvir(View view){
-        try {
-            Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-            //intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "pt-BR");
-            startActivityForResult(intent, 10);
-        }catch (Exception e){
-            tv = findViewById(R.id.mainResponse);
-            tv.setText(e.getMessage());
-        }
-    }
-
 }
