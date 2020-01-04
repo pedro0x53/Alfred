c4c32b5860f74b3c3e113f1f2b0a5dc68580928a
Pedro Sousa <pedrosousa2708@gmail.com>
2020-01-04 18:25:16 -0300

    Classe com todas expressões regulares definidas.

---
+++ app/src/main/java/assistant/alfred/Utils/RegexStrings.java
@@ -0,0 +1,7 @@
+package assistant.alfred.Utils;
+
+public class RegexStrings {
+    public static String
+            REGEX_START_LISTEN = "^(?i)(ei alfred|olá alfred|oi alfred|alfred)$",
+            REGEX_RUN_APP = "^(?i)(iniciar|inicie|execute|executar|abrir)( o)?( app| aplicativo)?( )?([^app aplicativo].*)$";
+}
