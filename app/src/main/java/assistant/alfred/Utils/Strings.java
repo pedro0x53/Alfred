d3fbe310d85e36e411a3f71b0e247a61d88fc626
Pedro Sousa <pedrosousa2708@gmail.com>
2020-01-04 18:25:42 -0300

    Classe com todas as respostas definidas.

---
+++ app/src/main/java/assistant/alfred/Utils/Strings.java
@@ -0,0 +1,7 @@
+package assistant.alfred.Utils;
+
+public class Strings {
+    public static String
+            GREETEING = "Olá! Qual o seu nome?",
+            ERROR = "Desculpe, não consegui entender, pode repetir?";
+}
