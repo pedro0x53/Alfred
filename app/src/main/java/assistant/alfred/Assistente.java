b3dac8aa6ace3bebc53b0f308d4ec0342f2b6973
Pedro Sousa <pedrosousa2708@gmail.com>
2020-01-08 21:21:05 -0300

    Criação da função runApp, função startListen e construtor

--- app/src/main/java/assistant/alfred/Assistente.java
+++ app/src/main/java/assistant/alfred/Assistente.java
@@ -1,13 +1,57 @@
 package assistant.alfred;
 
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.widget.TextView;
 import java.util.ArrayList;
 
-public class Assistente {
+import assistant.alfred.Controllers.ActivityController;
+import assistant.alfred.Controllers.RegexController;
+import assistant.alfred.Utils.RegexStrings;
 
-    public void processar(ArrayList<String> data, TextView tv) {
-        String[] code = RegexController.getResponse(data);
-        String action = "Code: " + code[0] + "\nRequest: " + code[1];
-        tv.setText(action);
+class Assistente {
+    private ActivityController activityController;
+
+    Assistente(ActivityController activityController, PackageManager packageManager) {
+        this.activityController = activityController;
+    }
+
+    void startListen() {
+        activityController.startSpeechRecognizer();
+    }
+
+    void process(ArrayList<String> data, TextView tv) {
+        String[] res = RegexController.getResponse(data);
+        int code = Integer.parseInt(res[0]);
+        String listen = res[1];
+
+        switch (code) {
+            case 10:
+                break;
+            case 20:
+                runApp(listen, tv);
+                break;
+        }
+    }
+
+    private void runApp(String req, TextView tv) {
+        boolean status = false;
+        String res = "";
+        String appReq = req.replaceFirst(RegexStrings.REGEX_REPLACE_RUN_APP, "");
+        ArrayList<ApplicationInfo> appList = activityController.getInstalledApplications();
+        for(ApplicationInfo appInfo : appList) {
+            if (appInfo.packageName.contains(appReq.toLowerCase())) {
+                status = true;
+                activityController.run(appInfo);
+            }
+        }
+
+        if(status){
+            res = "Iniciando " + appReq;
+        }else {
+            res = "Ops! Não foi possível encontrar o app " + appReq;
+        }
+
+        tv.setText(res);
     }
 }
