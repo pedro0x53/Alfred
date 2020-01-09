74c1de1ed82aa5d243c1b11faca1414ef2b8be9b
Pedro Sousa <pedrosousa2708@gmail.com>
2020-01-08 21:17:17 -0300

    Atualização da resposta default (code) e correção do code de RUN_APP

--- app/src/main/java/assistant/alfred/Controllers/RegexController.java
+++ app/src/main/java/assistant/alfred/Controllers/RegexController.java
@@ -1,18 +1,18 @@
-package assistant.alfred;
+package assistant.alfred.Controllers;
 
 import java.util.ArrayList;
 import static assistant.alfred.Utils.RegexStrings.*;
 
-class RegexController {
+public class RegexController {
     public static String[] getResponse(ArrayList<String> reqList) {
-        String[] res = {"", ""};
+        String[] res = {"-1", ""};
         for(String req: reqList) {
             if(req.matches(REGEX_START_LISTEN)) {
                 res[0] = "10";
                 res[1] = req;
             }
             else if(req.matches(REGEX_RUN_APP)) {
-                res[0] = "10";
+                res[0] = "20";
                 res[1] = req;
             }
         }
