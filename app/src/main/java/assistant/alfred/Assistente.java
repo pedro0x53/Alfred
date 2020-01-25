26223a63926e48c2b6ed94113f168401f92925a2
Moyses <m.mirazev@gmail.com>
2020-01-25 21:32:23 +0000

    add condicionais de verificação de funções, funções de manipulação e funções relacionadas ao modo agendamento.

--- app/src/main/java/assistant/alfred/Assistente.java
+++ app/src/main/java/assistant/alfred/Assistente.java
@@ -1,7 +1,29 @@
 package assistant.alfred;
 
+import android.annotation.SuppressLint;
 import android.content.pm.ApplicationInfo;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.Scanner;
+
 
 import assistant.alfred.Controllers.ActivityController;
 import assistant.alfred.Controllers.MessageController;
@@ -9,7 +31,10 @@ import assistant.alfred.Controllers.RegexController;
 import assistant.alfred.Utils.Strings;
 import assistant.alfred.Utils.RegexStrings;
 
-class Assistente {
+
+class Assistente{
+
+
     private ActivityController activityController;
     private MessageController messageController;
 
@@ -35,27 +60,151 @@ class Assistente {
             case 20:
                 runApp(listen);
                 break;
+            case 30:
+                schedulingMode(listen);
+                break;
+            case 40:
+                schedule(listen);
+                break;
+            case 50:
+                verAgendamento(listen);
+                break;
+            case 1000:
+                cancelingFunction(listen);
+                break;
             case -1:
                 messageController.createMessage(Strings.ERROR, 2);
         }
     }
 
+
+
+
+    public void tutorial(){
+        messageController.createMessage(Strings.SALUTATION,2);
+        messageController.createMessage(Strings.TUTORIAL,2);
+        messageController.createMessage(Strings.NAMES_FUNCTIONS, 2);
+    }
     private void runApp(String req) {
         boolean status = false;
         String appReq = req.replaceFirst(RegexStrings.REGEX_REPLACE_RUN_APP, "");
         ArrayList<ApplicationInfo> appList = activityController.getInstalledApplications();
-        for(ApplicationInfo appInfo : appList) {
+        for (ApplicationInfo appInfo : appList) {
             if (appInfo.packageName.contains(appReq.toLowerCase())) {
                 status = true;
                 activityController.run(appInfo);
             }
         }
 
-        if(status){
+        if (status) {
             messageController.createMessage(Strings.STARTING_APP(appReq), 2);
-        }else {
+        } else {
             messageController.createMessage(Strings.STARTING_APP_FAILED(appReq), 2);
         }
 
     }
+
+
+    public void schedulingMode(String req) {
+        String string = req.toLowerCase();
+        if (string.contains("agendamento")){
+            messageController.createMessage(Strings.STARTING_AGENDAMENTO, 2);
+            messageController.createMessage(Strings.SELECTION_OPTIONS_AGENDAMENTO, 2);
+        } else {
+            messageController.createMessage(Strings.ERROR, 2);
+        }
+    }
+
+    private void verAgendamento(String req) {
+        String string = req.toLowerCase();
+        String day = string.replaceFirst(RegexStrings.REGEX_REPLACE_DAYS, "");
+        messageController.createMessage("Agendamento de " + day + ":", 2 );
+        leitor(day +".txt", string);
+            // agendado ou já existe
+            //ja existe : deseja marcar esse compromisso por cima?
+    }
+
+
+    public void schedule(String req) {
+        String string = req.toLowerCase();
+        String day = string.replaceFirst(RegexStrings.REGEX_REPLACE_CONFIRM_AGENDAMENTO_DAY , "");
+        escritor(day + ".txt", string);
+        messageController.createMessage("Tarefa salva com sucesso", 2);
+
+    }
+
+    private void cancelingFunction(String req) {
+        String string = req.replaceFirst(RegexStrings.REGEX_REPLACE_RUN_APP, "");
+        if (string.contains("agendamento")) {
+            messageController.createMessage("Modo a AGENDAMENTO CANCELADO", 2);
+        } else {
+            messageController.createMessage("MODO AGENDAMENTO FECHADO", 2);
+        }
+    }
+
+
+    private void leitor(String fileName, String frase) {
+        String conteudo = "";
+        try {
+            FileReader arq = new FileReader("/data/data/assistant.alfred/files/" + fileName);
+            BufferedReader lerArq = new BufferedReader(arq);
+            String linha= "";
+            try {
+                linha = lerArq.readLine();
+                while(linha != null){
+                    messageController.createMessage(linha,2);
+                    conteudo += linha + "";
+                    linha = lerArq.readLine();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void escritor(String fileName, String phrase ) {
+        String hours_1 = phrase.replaceFirst(RegexStrings.REGEX_REPLACE_CONFIRM_AGENDAMENTO_HOURS_1,"");
+        String hours_2 = hours_1.replaceFirst(RegexStrings.REGEX_REPLACE_CONFIRM_AGENDAMENTO_HOURS_2,"");
+        String action = phrase.replaceFirst(RegexStrings.REGEX_REPLACE_CONFIRM_AGENDAMENTO_ACTION, "");
+        String day = phrase.replaceFirst(RegexStrings.REGEX_REPLACE_CONFIRM_AGENDAMENTO_DAY,"");
+        String frase = "Action: ["+ action + "] "+" Hours: ["+ hours_2 +"].";
+
+        try {
+            FileWriter arquivo = new FileWriter("/data/data/assistant.alfred/files/" + fileName, true);
+            PrintWriter gravarArq = new PrintWriter(arquivo);
+            gravarArq.println(frase);
+            gravarArq.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            messageController.createMessage(e.toString(), 2);
+            messageController.createMessage("erro  aqui", 2);
+        }
+    }
+
+    public void verifyFiles(){
+            String[] files = new String[7];
+            files[0] = "domingo.txt";
+            files[1] = "segunda.txt";
+            files[2] = "terça.txt";
+            files[3] = "quarta.txt";
+            files[4] = "quinta.txt";
+            files[5] = "sexta.txt";
+            files[6] = "sábado.txt";
+
+
+            for (String file: files) {
+                try {
+                    FileWriter arquivo = new FileWriter("/data/data/assistant.alfred/files/" + file);
+                    arquivo.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    messageController.createMessage(e.toString(), 2);
+                }
+            }
+        }
+
+
+
 }
