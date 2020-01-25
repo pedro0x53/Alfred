7e4e204f1f9f759bf5d4b693ae9947627293278b
Moyses <m.mirazev@gmail.com>
2020-01-25 21:35:40 +0000

    add novo toolbar, ação aos botões do menu, check arquivos txt ao iniciar programa

--- app/src/main/java/assistant/alfred/MainActivity.java
+++ app/src/main/java/assistant/alfred/MainActivity.java
@@ -1,13 +1,22 @@
 package assistant.alfred;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+
 import java.util.ArrayList;
 import assistant.alfred.Controllers.ActivityController;
 
@@ -19,25 +28,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         ActivityController activityController = new ActivityController(mainActivity);
+        Toolbar toolbar =  findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         a = new Assistente(activityController);
-
-
-
+        a.verifyFiles();
         ImageButton mic = findViewById(R.id.mic);
         mic.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 try {
                     a.startListen();
+
                 }
                 catch (Exception e) {
                     System.out.println(e.getMessage());
                 }
             }
         });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_tutorial) {
+            a.tutorial();
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
