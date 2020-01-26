e3432c6e6b99cbb9d01c3dd5af7798818beb809e
Pedro Sousa <pedrosousa2708@gmail.com>
2020-01-26 09:45:07 -0300

    Adição de vetor de strings para tutorial; Ediçaõ de textos para melhorar a experiência.

--- app/src/main/java/assistant/alfred/Utils/Strings.java
+++ app/src/main/java/assistant/alfred/Utils/Strings.java
@@ -2,37 +2,37 @@ package assistant.alfred.Utils;
 
 public class Strings {
     public static String
-            GREETEING = "Olá! Qual o seu nome?",
             ERROR = "Desculpe, não consegui entender, pode repetir?",
 
-    //tutorial:
-            SALUTATION = "Oi, meu nome é Alfred, sou seu mais novo assistente de voz.\n",
-            TUTORIAL =  "Minhas funcionalidades são adicionadas e atualizadas a cada patch de atualização, pelos meus desenvolvedores.\n" +
-                        "Eu possuo dois tipos de funções, simples e compostas. As funções simples são ativadas com uma simples frase.\n" +
-                        "As funções compostas possuem instruições dentro delas quando convocadas.\n",
-            NAMES_FUNCTIONS=  "Funções Disponiveis: \n" +
-                        "Para abrir algum aplicativo, fale:\n  ºAbrir NOME DO APLICATIVO.\n" +
-                        "Para organizar suas tarefas, fale :\n  ºModo agendamento.",
-
-    //agendamento:
+    // Agendamento:
             STARTING_AGENDAMENTO = "Modo Agendamento",
-            SELECTION_OPTIONS_AGENDAMENTO = "Para Agendar fale:\n  ºAção ás Horas de Dia da Semana.\n" +
-                                            "Para Ver Agendamentos fale:\n  ºVer Agendamentos de Dia da Semana.\n" +
-                                            "Sair Modo Agendamento fale:\n  ºCancelar modo agendamento.",
+            SELECTION_OPTIONS_AGENDAMENTO = "Para Agendar fale:\nNOME DO EVENTO ás HORA de DIA DA SEMANA.\n" +
+                                            "Para Ver Agendamentos fale:\n- Ver Agendamentos de Dia da Semana.\n" +
+                                            "Sair Modo Agendamento fale:\n- Cancelar modo agendamento.",
             CONFIRMED_AGENDAMENTO = "Agendamento confirmado.",
             OPTION_VER_AGENDAMENTOS="Opção VER AGENDAMENTO selecionada",
             VER_AGENDAMENTO = "Fale o dia da semana que deseja ver agendamentos.",
-            TUTORIAL_AGENDAMENTO = "Fale: 'AÇÂO ás HORAS de DIA DA SEMANA'";
+            TUTORIAL_AGENDAMENTO = "Fale: 'AÇÃO ás HORAS de DIA DA SEMANA'";
 
+    // Tutorial
+    public static String[] TUTORIAL = {
+            "Oi, meu nome é Alfred, sou seu mais novo assistente de voz.",
+            "Minhas funcionalidades são adicionadas e atualizadas a cada patch de atualização, pelos meus desenvolvedores.",
+            "Eu possuo dois tipos de funções, simples e compostas.",
+            "As funções simples são ativadas com uma simples frase.",
+            "As funções compostas possuem instruições dentro delas quando convocadas.",
+            "Funções Disponiveis: ",
+            "[SIMPLES] Para abrir algum aplicativo, fale: \nAbrir/Iniciar/Executar o NOME DO APLICATIVO.",
+            "[COMPOSTA] Para organizar suas tarefas, fale: \n\"Modo agendamento\" e siga as instruções."
+    };
 
-
+    // Strings dinamicas
     public static String STARTING_APP(String APP_NAME){
         return "Iniciando " + APP_NAME;
     }
     public static String STARTING_APP_FAILED(String APP_NAME){
         return "Ops! Não foi possível encontrar o app " + APP_NAME;
     }
-
     public static String Exit(String FUNCTION_NAME){
         return "Saindo: " + FUNCTION_NAME;
     }
