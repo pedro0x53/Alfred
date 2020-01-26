5afa956271d23daff555bb44c893c8f4d28f9d79
Pedro Sousa <pedrosousa2708@gmail.com>
2020-01-26 09:54:23 -0300

    Separação de escopo para funções de scroll

--- app/src/main/java/assistant/alfred/Controllers/MessageController.java
+++ app/src/main/java/assistant/alfred/Controllers/MessageController.java
@@ -3,6 +3,7 @@ package assistant.alfred.Controllers;
 import android.app.Activity;
 import android.view.Gravity;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import assistant.alfred.R;
@@ -11,10 +12,12 @@ public class MessageController {
 
     private Activity context;
     private LinearLayout chat;
+    private ScrollView scrollView;
 
     public MessageController(Activity context) {
         this.context = context;
         chat = context.findViewById(R.id.chat);
+        scrollView = context.findViewById(R.id.scrollView);
     }
 
     public void createMessage(String content, int code) {
@@ -40,6 +43,32 @@ public class MessageController {
 
         message.setLayoutParams(params);
         message.setText(content);
+
         chat.addView(message);
+
+        scrollDown();
+
+    }
+
+    public void clearMessages() {
+        chat.removeAllViews();
+    }
+
+    public void scrollDown() {
+        scrollView.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                scrollView.fullScroll(ScrollView.FOCUS_DOWN);
+            }
+        }, 100L);
+    }
+
+    public void scrollUp() {
+        scrollView.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                scrollView.fullScroll(ScrollView.FOCUS_UP);
+            }
+        }, 100L);
     }
 }
